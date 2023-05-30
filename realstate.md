7521475d08bee7676fbce9104fd24a4c13d41d76
Enrique <enrique.sousa@gmail.com>
2023-05-30 09:58:17 -0700

    33. Frontend Template Setup Part 2

--- realstate.md
+++ realstate.md
@@ -1008,6 +1008,76 @@ me estaba faltando el asset:
 ```
 
 También actualice todos las {{ asset('frontend/) }} para todas las images de la pagina, que son como 50!
+## 33. Frontend Template Setup Part 2
+Segmentar en secciones.
+En resources/views/frontend/frontend_dashboard.blade.php:
+```php
+<body>
+
+    <div class="boxed_wrapper">
+
+        <!-- preloader -->
+        @include('frontend.home.preload')
+
+        <!-- main header -->
+        @include('frontend.home.header')
+
+        <!-- Mobile Menu  -->
+        @include('frontend.home.mobile_menu')
+
+
+        {{-- Sección principal --}}
+        @yield('main')
+
+        <!-- main-footer -->
+        @include('frontend.home.footer')
+
+
+        <!--Scroll to top-->
+        <button class="scroll-top scroll-to-target" data-target="html">
+            <span class="fal fa-angle-up"></span>
+        </button>
+
+    </div>
+
+
+    <!-- jequery plugins -->
+    <script src="{{ asset('frontend/assets/js/jquery.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/owl.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/wow.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/validation.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/jquery.fancybox.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/appear.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/scrollbar.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/isotope.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/jQuery.style.switcher.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/jquery-ui.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/nav-tool.js') }}"></script>
+
+    <!-- main-js -->
+    <script src="{{ asset('frontend/assets/js/script.js') }}"></script>
+
+</body> 
+```
+
+En resources/views/frontend/index.blade.php
+Ponemos en main content
+```php
+@extends('frontend.frontend_dashboard')
+
+@section('main')
+    ...
+@endsection
+```
+
+
+
+
+
+
 
 
 
---
+++ resources/views/frontend/home/preload.blade.php
@@ -0,0 +1,59 @@
+<!-- preloader -->
+<div class="loader-wrap">
+    <div class="preloader">
+        <div class="preloader-close"><i class="far fa-times"></i></div>
+        <div id="handle-preloader" class="handle-preloader">
+            <div class="animation-preloader">
+                <div class="spinner"></div>
+                <div class="txt-loading">
+                    <span data-text-preloader="F" class="letters-loading">
+                        F
+                    </span>
+                    <span data-text-preloader="O" class="letters-loading">
+                        O
+                    </span>
+                    <span data-text-preloader="T" class="letters-loading">
+                        T
+                    </span>
+                    <span data-text-preloader="O" class="letters-loading">
+                        O
+                    </span>
+                    <span data-text-preloader="S" class="letters-loading">
+                        S
+                    </span>
+                    <span data-text-preloader="/" class="letters-loading">
+                        /
+                    </span>
+                    <span data-text-preloader="O" class="letters-loading">
+                        O
+                    </span>
+                    <span data-text-preloader="F" class="letters-loading">
+                        F
+                    </span>
+                    <span data-text-preloader="I" class="letters-loading">
+                        I
+                    </span>
+                    <span data-text-preloader="C" class="letters-loading">
+                        C
+                    </span>
+                    <span data-text-preloader="I" class="letters-loading">
+                        I
+                    </span>
+                    <span data-text-preloader="A" class="letters-loading">
+                        A
+                    </span>
+                    <span data-text-preloader="L" class="letters-loading">
+                        L
+                    </span>
+                    <span data-text-preloader="E" class="letters-loading">
+                        E
+                    </span>
+                    <span data-text-preloader="S" class="letters-loading">
+                        S
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- preloader end -->
